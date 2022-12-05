10bbbab0f3dc845797e3d937e13689b632961a3f
Andrea Sanchez <andrea.sanchez@ufl.edu>
2022-12-05 15:40:49 -0500

    merge function

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <sstream>
 #include <fstream>
+#include <iomanip>
 using namespace std;
 
 void GetData(string filepath, vector<Houses>& housesset)
@@ -53,7 +54,21 @@ void GetData(string filepath, vector<Houses>& housesset)
     }
 
 }
-void mergeSortOnPrice(){
+void mergePrice(vector<Houses>& vec, int left, int middle, int right)
+{
+
+
+}
+void mergeSortOnPrice(vector<Houses>& vec, int begin, int end){
+    int mid = 0;
+    int add = begin + end;
+    if(begin < end) // it will end when they both equal each other
+    {
+        mid = add/2;
+        mergeSortOnPrice(vec, begin, mid); // performs merge sort on the first half of the vector
+        mergeSortOnPrice(vec, mid + 1, end); // performs merge sort on the second half of the vector
+        mergePrice(vec, begin, mid, end);
+    }
 
 }
 void mergeSortOnLocation()
@@ -64,10 +79,7 @@ void mergeLocation()
 {
 
 }
-void mergePrice()
-{
 
-}
 void shellSortOnPrice()
 {
 
@@ -82,10 +94,10 @@ int main() {
     string ignore;
     int prioritySelection = 0;
     //ask for priority if either the user want prefers price or location
-    cout << "Welcome to your Real Estate Ranker!" << endl;
+    cout << std::setw(32) << "Welcome to your Real Estate Ranker!" << std::setw(32) << endl;
     cout << "In this ranker, we will display your top 5 home option based on your priority" << endl;
     cout << "The priorities that you can select are either price or location nearest you" << endl;
-    cout << "ARE YOU READY?" << endl;
+    cout << setw(8) <<"ARE YOU READY?" << endl;
     cin >> ignore;
     cout << "Which priority do you prefer? Price(insert 1) or Location(insert 2)" << endl;
     cin >> prioritySelection;
@@ -95,6 +107,7 @@ int main() {
     if(prioritySelection == 1)
     {
         //call merge and shell sort on price
+
 
     }
 
