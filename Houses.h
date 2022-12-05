6416b41223261186214e20af7721f1a001620d56
Andrea Sanchez <andrea.sanchez@ufl.edu>
2022-12-05 00:59:44 -0500

    adding houses header

---
+++ Houses.h
@@ -0,0 +1,17 @@
+
+#ifndef REALESTATERANKER_HOUSES_H
+#define REALESTATERANKER_HOUSES_H
+
+#endif //REALESTATERANKER_HOUSES_H
+#pragma once
+#include <string>
+using namespace std;
+class Houses{
+    string address;
+    int price;
+    int radius;
+    int beds;
+    int baths;
+public:
+    Houses()
+};
